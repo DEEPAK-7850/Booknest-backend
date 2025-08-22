6eec3bb929efe52676fe2c7aba11e413956b3628
DEEPAK-7850 <deepaklakhara151@gmail.com>
2025-08-23 01:06:33 +0530

    final commit

--- index.js
+++ index.js
@@ -6,26 +6,19 @@ import cors from "cors";
 import bookRoute from "./route/book.route.js";
 import userRoute from "./route/user.route.js";
 
-const app = express();
+dotenv.config();
 
+const app = express();
 app.use(cors());
 app.use(express.json());
 
-dotenv.config();
-
-const PORT = process.env.PORT || 4000;
-const URI = process.env.Mongo_URI;
+const PORT = process.env.PORT || 4001;
+const URI = process.env.MONGO_URI;
 
 // connect to mongoDB
-try {
-    mongoose.connect(URI, {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-    });
-    console.log("Connected to mongoDB");
-} catch (error) {
-    console.log("Error: ", error);
-}
+mongoose.connect(URI)
+  .then(() => console.log("✅ Connected to mongoDB"))
+  .catch((error) => console.error("❌ MongoDB connection error:", error));
 
 // defining routes
 app.use("/book", bookRoute);
